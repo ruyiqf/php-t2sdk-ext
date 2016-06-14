142c0b5236b2304d42ae029410d7cacea7ae17ae
caizixin <caizixinmail@gmail.com>
2016-06-14 18:28:01 +0800

    commit

--- source/t2connection.h
+++ source/t2connection.h
@@ -4,6 +4,7 @@
 #include <Include/t2sdk_interface.h>
 #include "source/CCallback.h"
 #include "source/t2sdk_struck.h"
+#include "php_t2sdk.h"
 #include <dlfcn.h> 
 
 typedef CConfigInterface* (*config)(); 
