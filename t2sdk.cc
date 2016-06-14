cfdb3462793e173add0927184a49af0c8b8021ad
caizixin <caizixinmail@gmail.com>
2016-06-14 19:00:47 +0800

    commit

--- t2sdk.cc
+++ t2sdk.cc
@@ -4,6 +4,11 @@ ZEND_DECLARE_MODULE_GLOBALS(t2sdk);
 zend_class_entry *t2connection_ce;
 zend_object_handlers t2connection_object_handlers;
 
+typedef CConfigInterface* (*config)(); 
+typedef CConnectionInterface* (*connection)(CConfigInterface*); 
+typedef IF2Packer* (*packer)(int); 
+typedef IF2UnPacker* (*unpacker)(void *, unsigned int);
+typedef IBizMessage* (*biz_message)();
 config T2NewConfig;
 connection T2NewConnection;
 packer T2NewPacker;
