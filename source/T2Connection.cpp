eebe420b51186ad1f925b0a2f0125239c7e19439
caizixin <caizixinmail@gmail.com>
2016-06-28 18:26:04 +0800

    commit

--- source/T2Connection.cpp
+++ source/T2Connection.cpp
@@ -163,7 +163,7 @@ zval* T2Connection::login(char *fund_account, char *password)
     IF2UnPacker *pUnPacker = NULL;
     int errorNo = 0;
     char errorMsg[] = "";
-    int iSystemNo = lp_SecuRequestMode->Login(fund_account, password, pUnPacker, errorNo, *errorMsg);
+    int iSystemNo = lp_SecuRequestMode->Login(fund_account, password, pUnPacker, errorNo, &errorMsg);
     if(iSystemNo == -1)
     {
         return errorToZval(errorNo, errorMsg);
